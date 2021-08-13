ef1bfb2a2852cdc5e1e7eab0dadb50013a76915e
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-08-12 21:55:06 -0700

    added some script to render weather data

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ var currentForecastEl = $('#currentForecast');
 
 var cityQuery;
 
-function CitySearchRender() {
+function citySearchRender() {
     citySearchEl.append(`
     <input type="search" class="form-control" id="cityQuery">
     <button type="submit" class ="btn" id="searchButton">Search</button>
     `)
 }
 
-CitySearchRender();
+function currentForecastRender() {
+    currentForecastEl.append(`
+    <div id="current">
+        <h3>${cityQuery}</h3>
+        <p>Temp:</p>
+        <p>Wind:</p>
+        <p>Humidity:</p>
+        <p>UV Index:</p>
+    </div>
+    <div id="forecast">
+        <h3>Five day forecast:</h3>
+    </div>
+    `)
+}
+
+citySearchRender();
