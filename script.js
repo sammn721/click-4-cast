2df922c75630b1808ad5eb5012ac6598955e5497
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-08-12 21:33:21 -0700

    was calling script before jquery, fixed

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var openWeatherKey = "b630f221bd1295ec02183f26749a279c";
 var openWeatherURL = "https://api.openweathermap.org/data/2.5/";
 
-var citySearchEl = document.getElementById('citySearch');
-var searchedCitiesEl = document.getElementById('searchedCities');
-var currentForecastEl = document.getElementById('currentForecast');
+var citySearchEl = $('#citySearch');
+var searchedCitiesEl = $('#searchedCities');
+var currentForecastEl = $('#currentForecast');
 
 var cityQuery;
+
+function CitySearchRender() {
+    citySearchEl.append(`
+    <input type="search" class="form-control" id="cityQuery">
+    <button type="submit" class ="btn" id="searchButton">Search</button>
+    `)
+}
+
+CitySearchRender();
