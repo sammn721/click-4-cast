01a52dad56de21a190cc483bc28839dca22e5414
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-08-12 21:02:56 -0700

    script.js reset

--- script.js
+++ script.js
@@ -1,53 +1,8 @@
-var fetchButton = document.getElementById('fetch-button');
-var cityInput = document.getElementById('city-input');
-var cityButtons = document.getElementById('city-buttons');
+var openWeatherKey = "b630f221bd1295ec02183f26749a279c";
+var openWeatherURL = "https://api.openweathermap.org/data/2.5/";
 
+var citySearchEl = document.getElementById('citySearch');
+var searchedCitiesEl = document.getElementById('searchedCities');
+var currentForecastEl = document.getElementById('currentForecast');
 
-function getAPI() {
-    var userCity = userInput.value;
-    var currentWeatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=" + openWeatherKey;
-    var latitude;
-    var longitude;
-    var onecallQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + openWeatherKey;
-    fetch(currentWeatherQueryURL)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-    });
-    fetch(onecallQueryURL)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-    });
-    
-}
-
-// function forecast() {
-    
-// }
-
-cityInput.addEventListener('submit', function(event) {
-    event.preventDefault();
-    getAPI();
-    // forecast();
-});
-
-fetchButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    getAPI();
-    // forecast();
-});
-
-cityButtons.addEventListener("click", function(event) {
-    
-    
-    if (event.target.matches("button")) {
-        var clickedCity = JSON.stringify(event.target.id);
-        console.log(clickedCity);
-
-    }
-});
+var cityQuery;
